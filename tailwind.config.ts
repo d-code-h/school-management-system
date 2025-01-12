ffa9da1f2c81f8980d366cae16d12857584193de
Yunus Habeeb <habeebdh1@gmail.com>
2025-01-12 19:31:17 +0100

    Refactor: update dependencies and enhance layout with toast notifications

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,24 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        Sky: '#C3EBFA',
+        SkyLight: '#EDF9FD',
+        Purple: '#CFCEFF',
+        PurpleLight: '#F1F0FF',
+        Yellow: '#FAE27C',
+        YellowLight: '#FEFCE8',
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
